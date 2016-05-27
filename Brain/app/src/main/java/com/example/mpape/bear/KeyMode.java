f881f312b05333e1dc2e0f1ac4118e81363b1750
mpaperboat <mpaperboat@outlook.com>
2016-05-27 09:17:06 +0800

    摄像头初步完成

--- Brain/app/src/main/java/com/example/mpape/bear/KeyMode.java
+++ Brain/app/src/main/java/com/example/mpape/bear/KeyMode.java
@@ -4,8 +4,12 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Paint;
+import android.media.Image;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
@@ -21,11 +25,33 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Semaphore;
+//import java.util.logging.Handler;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+
 
 public class KeyMode extends Activity implements SurfaceHolder.Callback {
     private static Context context = null;
@@ -39,8 +65,11 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
     private InputStream mmInputStream;
     private UUID uuid;
     private int bconnected;
+    private int activepohoto;
+    private Semaphore ims;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activepohoto=0;
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -78,6 +107,20 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
                 } catch (Exception e) {
                     setdbg("Bluetooth Error");
                 }
+            }
+        });
+        Button button8 = (Button) findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                    new Thread(new Runnable(){
+                        public void run() {
+                           lettherebewifi();
+                        }
+
+                    }).start();
+
+
             }
         });
         final ImageButton ibutton = (ImageButton) findViewById(R.id.button6);
@@ -151,6 +194,130 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
         surfaceholder = surfaceview.getHolder();
         surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         surfaceholder.addCallback(this);
+        mHandler = new Handler() {
+            public void handleMessage(Message msg) {
+                // 更新UI
+                switch (msg.what) {
+                    case 1:
+                        setdbg2("WiFi On");
+                        break;
+                    case 0:
+                        setdbg2("WiFi Error");
+                        break;
+                }
+
+            }};
+        new Thread(new Runnable(){
+            public void run() {
+                lettherebewifi();
+            }
+
+        }).start();
+        //activepohoto=1;
+        ims=new Semaphore(1);
+        try {
+            ss = new ServerSocket(6000);
+        }catch (Exception e){
+
+        }
+        new Thread(new Runnable(){
+            public void run() {
+                while(true) {
+                    if(activepohoto==1)
+                    try {
+
+                        Socket s = ss.accept();
+                        System.out.println("连接成功!");
+                        ins = s.getInputStream();
+                        //ims.acquire();
+                       image = InputStream2Bitmap(ins);
+                       // System.out.print("haha"+image.getBounds().toString());
+                        //ims.release();
+
+                        ins.close();
+                        //ss.close();
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }).start();
+    }
+    private Handler mHandler;
+    private int hahap=0;
+        private ServerSocket ss;
+       private Bitmap image;
+       private InputStream ins;
+    private Handler dh2=null;
+    public Drawable bitmap2Drawable(Bitmap bitmap) {
+                BitmapDrawable bd = new BitmapDrawable(bitmap);
+               Drawable d = (Drawable) bd;
+                return d;
+            }
+
+    public Bitmap InputStream2Bitmap(InputStream is) {
+                return BitmapFactory.decodeStream(is);
+            }
+
+    public Drawable InputStream2Drawable(InputStream is) {
+                Bitmap bitmap = this.InputStream2Bitmap(is);
+        return this.bitmap2Drawable(bitmap);
+         }
+
+    public void lettherebewifi(){
+        Message msg = new Message();
+        //给message对象赋值
+
+        //发送message值给Handler接收
+
+
+        try {
+
+            Socket socket = new Socket("192.168.43.1", 8888);
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.write(getLocAddress() + "\n");
+            pw.flush();
+            //pw.close();
+            socket.close();
+            msg.what = 1;
+            mHandler.sendMessage(msg);
+        } catch (Exception e) {
+            msg.what = 0;
+
+            mHandler.sendMessage(msg);
+        }
+    }
+    public String getLocAddress(){
+
+        String ipaddress = "";
+
+        try {
+            Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            // 遍历所用的网络接口
+            while (en.hasMoreElements()) {
+                NetworkInterface networks = en.nextElement();
+                // 得到每一个网络接口绑定的所有ip
+                Enumeration<InetAddress> address = networks.getInetAddresses();
+                // 遍历每一个接口绑定的所有ip
+                while (address.hasMoreElements()) {
+                    InetAddress ip = address.nextElement();
+                    if (!ip.isLoopbackAddress()
+                            && ip instanceof Inet4Address) {
+                        ipaddress = ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            Log.e("", "获取本地ip地址失败");
+            e.printStackTrace();
+        }
+
+        //System.out.println("本机IP:" + ipaddress);
+
+        return ipaddress;
+
     }
     void  checkblue(){
         if(!sendData("-")){
@@ -159,6 +326,10 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
     }
     void setdbg(String s){
         TextView dbg=(TextView)findViewById(R.id.textView);
+        dbg.setText(s);
+    }
+    void setdbg2(String s){
+        TextView dbg=(TextView)findViewById(R.id.textView3);
         dbg.setText(s);
     }
     String getdbg(){
@@ -178,12 +349,14 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
     @Override
     protected void onPause(){
         super.onPause();
+        activepohoto=0;
         surfaceview.setVisibility(View.GONE);
     }
     @Override
     protected void onResume(){
         super.onResume();
         surfaceview.setVisibility(View.VISIBLE);
+        activepohoto=1;
     }
     @Override
     public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
@@ -192,29 +365,7 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         System.out.println("surfacecreated");
-        try {
-            camera = Camera.open();
-            camera.setPreviewDisplay(holder);
-            Camera.Parameters parameters = camera.getParameters();
-            if (this.getResources().getConfiguration().orientation
-                    != Configuration.ORIENTATION_LANDSCAPE) {
-                parameters.set("orientation", "portrait");
-                camera.setDisplayOrientation(90);
-                parameters.setRotation(90);
-            } else {
-                parameters.set("orientation", "landscape");
-                camera.setDisplayOrientation(0);
-                parameters.setRotation(0);
-            }
-            camera.setParameters(parameters);
-            camera.startPreview();
-            System.out.println("camera.startpreview");
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            camera.release();
-            System.out.println("camera.release");
-        }
         try {
             mBTAdapter = BluetoothAdapter.getDefaultAdapter();
             Set<BluetoothDevice> pairedDevices = mBTAdapter.getBondedDevices();
@@ -230,14 +381,28 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
         }catch (Exception e){
             setdbg("Bluetooth Error");
         }
+        activepohoto=1;
+        new Thread(new Runnable(){
+            public void run() {
+                while(true) {
+                    if(activepohoto==1&&image!=null){
+                        Canvas c=surfaceholder.lockCanvas();
+                        if(c!=null){
+                            synchronized (surfaceholder) {
+                                c.drawBitmap(image, 0, 0, new Paint());
+                            }
+                            surfaceholder.unlockCanvasAndPost(c);
+                        }
+                    }
+                }
+            }
+
+        }).start();
     }
     @Override
     public void surfaceDestroyed(SurfaceHolder arg0) {
         System.out.println("surfaceDestroyed");
-        if (camera != null) {
-            camera.stopPreview();
-            camera.release();
-        }
+
         try {
             mmSocket.close();
         }catch (Exception e){
--- Eye/app/src/main/java/com/example/mpape/bear/KeyMode.java
+++ Eye/app/src/main/java/com/example/mpape/bear/KeyMode.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -21,9 +22,15 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import java.io.DataInputStream;
@@ -48,7 +55,26 @@ import java.util.concurrent.Executors;
 
 import java.util.concurrent.Semaphore;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class KeyMode extends Activity implements SurfaceHolder.Callback {
     private static Context context = null;
@@ -58,6 +84,9 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
     private String ping = "ping -c 1 -w 0.5 " ,mp;//
     private static final String TAG="TestTag";
     private int j;//
+    private ServerSocket sever;
+    private Semaphore semabrainip;
+    private String brainip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,15 +99,35 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
         surfaceholder = surfaceview.getHolder();
         surfaceholder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         surfaceholder.addCallback(this);
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //setdbg(getLocAddress());
-                scan();
-                setdbg(server);
+        try{
+            sever=new ServerSocket(8888);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        new Thread(new Runnable() {
+            public void run() {
+                while(true) {
+                    try {
+                        System.out.println("wifi"+"waiting!");
+                        Socket socket = sever.accept();
+                        BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream(),"UTF-8"));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                        String str=in.readLine();
+                        //str=in.readLine();
+                        System.out.println("wifi"+str);
+                        System.out.println("wifi"+str.length());
+                        semabrainip.acquire();
+                        brainip=str;
+                        semabrainip.release();
+                        socket.close();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
             }
-        });
-        mp=getLocAddress();
+        }).start();
+        semabrainip=new Semaphore(1);
+        //brainip="";
     }
     public String getLocAddress(){
 
@@ -280,8 +329,35 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
                 camera.setDisplayOrientation(0);
                 parameters.setRotation(0);
             }
+            //List<int[]> tmp=parameters.getSupportedPreviewFpsRange();
+            //System.out.println(parameters.getSupportedPreviewFpsRange().size());
+           // for(int i=1;i==1;);
+            parameters.setPreviewFpsRange(15000, 15000);
+          //  parameters.setPreviewFrameRate(2);
             camera.setParameters(parameters);
+            camera.setPreviewCallback(new Camera.PreviewCallback(){
+
+                public void onPreviewFrame(byte[] data, Camera camera) {
+                    Size size = camera.getParameters().getPreviewSize();
+                    try {
+                        // 调用image.compressToJpeg（）将YUV格式图像数据data转为jpg格式
+                        YuvImage image = new YuvImage(data, ImageFormat.NV21, size.width, size.height, null);
+                        if (image != null) {
+                            ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+                            image.compressToJpeg(new Rect(0, 0, size.width, size.height), 80, outstream);
+                            outstream.flush();
+                            // 启用线程将图像数据发送出去
+                            Thread th = new MyThread(outstream, brainip);
+                            th.start();
+                        }
+                    } catch (Exception ex) {
+                        Log.e("Sys", "Error:" + ex.getMessage());
+                    }
+                }
+
+            });
             camera.startPreview();
+            camera.autoFocus(null);
             System.out.println("camera.startpreview");
 
         } catch (IOException e) {
@@ -298,4 +374,40 @@ public class KeyMode extends Activity implements SurfaceHolder.Callback {
             camera.release();
         }
     }
+}
+
+class MyThread extends Thread {
+    private byte byteBuffer[] = new byte[1024];
+    private OutputStream outsocket;
+    private ByteArrayOutputStream myoutputstream;
+    private String ipname;
+
+    public MyThread(ByteArrayOutputStream myoutputstream, String ipname) {
+        this.myoutputstream = myoutputstream;
+        this.ipname = ipname;
+        try {
+            myoutputstream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            // 将图像数据通过Socket发送出去
+            Socket tempSocket = new Socket(ipname, 6000);
+            outsocket = tempSocket.getOutputStream();
+            ByteArrayInputStream inputstream = new ByteArrayInputStream(myoutputstream.toByteArray());
+            int amount;
+            while ((amount = inputstream.read(byteBuffer)) != -1) {
+                outsocket.write(byteBuffer, 0, amount);
+            }
+            myoutputstream.flush();
+            myoutputstream.close();
+            tempSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
